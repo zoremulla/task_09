9af787d70a532e61c32e207ee25773c7868bf2b9
DarthHamza <hamzamakia@gmail.com>
2018-04-17 19:11:38 +0300

    completed tests and task 09

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Restaurant
+from django.contrib.auth.models import User
 
 class RestaurantForm(forms.ModelForm):
     class Meta:
@@ -10,3 +11,17 @@ class RestaurantForm(forms.ModelForm):
         	'opening_time': forms.TimeInput(attrs={'type':'time'}),
         	'closing_time': forms.TimeInput(attrs={'type':'time'}),
         }
+
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email' ,'password']
+
+        widgets={
+        'password': forms.PasswordInput(),
+        }
+
+class SigninForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, widget=forms.PasswordInput())
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,46 @@
 from django.shortcuts import render, redirect
 from .models import Restaurant
-from .forms import RestaurantForm
+from .forms import RestaurantForm, SignupForm, SigninForm
+from django.contrib.auth import login, authenticate, logout
+
+def signup(request):
+    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            user.set_password(user.password)
+            user.save()
+
+            login(request, user)
+            return redirect("restaurant-list")
+    context = {
+        "form":form,
+    }
+    return render(request, 'signup.html', context)
+
+def signin(request):
+    form = SigninForm()
+    if request.method == 'POST':
+        form = SigninForm(request.POST)
+        if form.is_valid():
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            auth_user = authenticate(username=username, password=password)
+            if auth_user is not None:
+                login(request, auth_user)
+                return redirect('restaurant-list')
+    context = {
+        "form":form
+    }
+    return render(request, 'signin.html', context)
+
+def signout(request):
+    logout(request)
+    return redirect("signin")
 
 def restaurant_list(request):
     context = {
